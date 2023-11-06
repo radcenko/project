dfcd286d2d30d59af964ff838e821301904de56e
Olegs Radcenko <olegs.radcenko@gmail.com>
2023-11-06 20:56:28 +0100

    Task 3.1 done

--- routes/pictures.js
+++ routes/pictures.js
@@ -9,6 +9,26 @@ router.get('/', function(req, res, next) {
   res.render('pictures', { pictures: pictures});
 });
 
+// Route for serving a single picture by name
+router.get('/:pictureName', function(req, res, next) {
+  const pictureName = req.params.pictureName;
+  const picturePath = path.join(__dirname, '../pictures/', pictureName);
+
+  // Check if the file exists
+  if (fs.existsSync(picturePath)) {
+    // Serve the image file
+    res.sendFile(picturePath);
+  } else {
+    // If the file does not exist, send a 404 response
+    res.status(404).send('Picture not found');
+  }
+});
+
+// router.get('/:pictureName', function(req, res, next) {
+//   res.render('pictureDetails', { picture: req.params.pictureName });
+// });
+
+
 //display a file from the request in the logs and save in folder
 router.post('/', function(req, res, next) {
     const file = req.files.file;
